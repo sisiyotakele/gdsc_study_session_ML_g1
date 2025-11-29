e0eb5ec5872f191aff8e146d125651358e66aa95
Sisiyo T <sisiyohirpa@gmail.com>
2025-11-29 21:54:57 +0300

    Todays task

---
+++ task.py
@@ -0,0 +1,11 @@
+import numpy as np
+numbers = [10, 20, 30, 40, 50]
+arr = np.array(numbers)
+mean_value = arr.mean()
+max_value = arr.max()
+sum_value = arr.sum()
+
+print("Array:", arr)
+print("Mean:", mean_value)
+print("Maximum Value:", max_value)
+print("Sum:", sum_value)
